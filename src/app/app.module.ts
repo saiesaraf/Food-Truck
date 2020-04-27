ee77666b95c96834fb4ae10c655ac5b65f6f1fcd
saiesaraf <saie.saraf@gmail.com>
2020-04-26 20:28:11 -0400

    added login register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,12 +9,24 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { MenuComponent } from './menu/menu.component';
 import { AddMenuComponent } from './add-menu/add-menu.component';
+import { RouterModule} from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MDBBootstrapModule, IconsModule } from 'angular-bootstrap-md';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    AddMenuComponent
+    AddMenuComponent,
+    NavBarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +35,14 @@ import { AddMenuComponent } from './add-menu/add-menu.component';
     MatFormFieldModule,
     MatInputModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MDBBootstrapModule.forRoot(),
+    IconsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'login', component : LoginComponent },
+      { path: 'Register', component : RegisterComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
