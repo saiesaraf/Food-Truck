d11f28e4e0430c7e763080f6fdaa8049f3d72e3a
saiesaraf <saie.saraf@gmail.com>
2020-09-12 02:11:34 -0400

    added slider

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -25,16 +25,17 @@ export class BackgroundComponent implements OnInit {
     image: '../../assets/img/slider/slider-3.jpg',
     thumbImage: '../../assets/img/slider/slider-3.jpg',
     title: 'Delicious Blueberry PanCakes'
-  }
-  ,{
-    image: '../../assets/img/slider/american-heritage-chocolate-Id8BO472TbY-unsplash.jpg',
-    thumbImage: '../../assets/img/slider/american-heritage-chocolate-Id8BO472TbY-unsplash.jpg',
-    title: 'Delicious Blueberry PanCakes'
-  },
+  } ,
   {
-    image: '../../assets/img/slider/ilya-mashkov-_qxbJUr9RqI-unsplash.jpg',
-    thumbImage: '../../assets/img/slider/ilya-mashkov-_qxbJUr9RqI-unsplash.jpg',
-    title: 'Delicious Blueberry PanCakes'
-  }];
+    image: '../../assets/img/slider/slider-pizza.jpg',
+    thumbImage: '../../assets/img/slider/slider-pizza.jpg',
+    title: 'Pizza'
+  } ,
+  {
+    image: '../../assets/img/slider/pasta.jpg',
+    thumbImage: '../../assets/img/slider/pasta.jpg',
+    title: 'Pasta'
+  }
+  ];
 }
 }
